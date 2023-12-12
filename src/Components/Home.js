adb75be0b78577ad046887b34ff6da238c0c809c
Douglas Marcondes <71682660+dmarck82@users.noreply.github.com>
2023-12-12 18:02:43 -0300

    Últimos ajustes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,32 +1,37 @@
-function Home(){
-    return(
-        <div>
-            <div>
-                <p>Aluno: Douglas Oliveira Marcondes Cardoso - 2043351</p>
-                <p>Professor: Ricardo Sobjak</p>
-                <p>Tecnologias utilizadas:</p>
-                <li>Spring Boot</li>
-                <li>Node.Js</li>
-                <li>Postgres</li>
-                <li>MongoDb</li>
-                <li>React</li>
-            </div>
-            <br></br>
-            <div>
-                <p>Melhorias que podem ser realizadas:</p>
-                <li>Colocar o cadastro de pessoas junto com a página de login.</li>
-            </div>
-            <br></br>
-            <div>
-                <p>Bugs Detectados:</p>
-                <li>É necessário ter um usuário cadastrado para fazer o login primeiro para então poder fazer os cadastros de terceiros e assim poder utilizá-los.</li>
-                <li>Está com um bug para colocar os passeios em um pacote pela interface web.</li>
-                <li>É possível cadastrar um pacote pelo backend, porém não é possível atualizar o mesmo.</li>
-            </div>
-        </div>
+import img from '../fogo.jpg';
 
-        
-    )
+function Home() {
+  const containerStyle = {
+    display: 'flex',
+    justifyContent: 'space-evenly', // ou 'space-around' ou 'space-evenly' conforme necessário
+  };
+
+  const divStyle = {
+    textAlign: 'center',
+    margin: '10px',
+  };
+
+  return (
+    <div style={containerStyle}>
+    <div style={divStyle}>
+    <p>Aluno: Douglas Oliveira Marcondes Cardoso - 2043351</p>
+    <p>Professor: Ricardo Sobjak</p>
+    <p>Tecnologias utilizadas:</p>
+    <ul>
+        <li>Spring Boot</li>
+        <li>Node.Js</li>
+        <li>Postgres</li>
+        <li>MongoDb</li>
+        <li>React</li>
+    </ul>
+    </div>
+      <div style={divStyle}>
+        <h3>Agência de turismo paz e sossego</h3>
+        <img src={img} alt="Pousada pegando fogo" />
+      </div>
+      
+    </div>
+  );
 }
 
 export default Home;
